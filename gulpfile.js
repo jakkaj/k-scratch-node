bd90ef85b9099183c24652bba942e3edc4baebb0
Jordan Knight <jakkaj@gmail.com>
2017-05-22 07:49:26 +1000

    cleaning up prepublish code.

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,26 @@ var ava = require('gulp-ava');
 var del = require('del');
 var dest_test = "build/output_test";
 var fs = require('fs');
-var lec = require('gulp-line-ending-corrector');
+
 var run = require('gulp-run');
+var crlf = require('crlf');
 
-gulp.task('preparePublish', ['crlf'], function(){
-  
+gulp.task('preparePublish', ['version'], function(){  
+  return gulp.start('crlf');
 });
-
-// gulp.task('version', function(){
-//   return run('npm version patch').exec();
-// });
 
 gulp.task('crlf', function(){
-  return gulp.src(['build/output/ks.js'])
-    .pipe(lec({verbose:true, eolc: 'LF'}))
+  return new Promise((good)=>{
+    crlf.set('./build/output/ks.js', 'LF', function(err, endingType) {
+      console.log(endingType); 
+    });
+  });
 });
 
+gulp.task('version', function(){
+  return run('npm version patch').exec();
+});  
+  
 gulp.task('test',["compile:tests"], function() {
     return gulp.src(dest_test + '/tests/runTests/*.js')
     .pipe(ava({verbose: true}));
